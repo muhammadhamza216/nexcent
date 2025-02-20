a254769149e082cbad3b8a38154add1ecb001bf3
muhammadhamza216 <165778207+muhammadhamza216@users.noreply.github.com>
2025-02-20 17:10:16 +0500

    changingfile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { BrowserRouter } from "react-router-dom"
 import Onepageapp from "./onepageapp"
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Onepageapp/>
-    </BrowserRouter>
+     <Onepageapp/>    
     </>
   )
 }
